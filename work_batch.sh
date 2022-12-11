7af03762c4eeee4d41f635fe209c36d2ac7cc120
DESKTOP-3HRJJET\gunwoo <gunwoohan@naver.com>
2022-12-11 21:49:06 +0900

    ADD: add experiment bash file

---
+++ work_batch.sh
@@ -0,0 +1,29 @@
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_2 --gpus 4 --num_workers 16 --batch_size 2 --learning_rate 0.01
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_4 --gpus 4 --num_workers 16 --batch_size 4 --learning_rate 0.01
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_8 --gpus 4 --num_workers 16 --batch_size 8 --learning_rate 0.01
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_16 --gpus 4 --num_workers 16 --batch_size 16 --learning_rate 0.01
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_2 --gpus 4 --num_workers 16 --batch_size 2 --learning_rate 0.0001
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_4 --gpus 4 --num_workers 16 --batch_size 4 --learning_rate 0.0001
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_8 --gpus 4 --num_workers 16 --batch_size 8 --learning_rate 0.0001
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_16 --gpus 4 --num_workers 16 --batch_size 16 --learning_rate 0.0001
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_2 --gpus 4 --num_workers 16 --batch_size 2 --learning_rate 0.01 --gan_weight 0.5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_4 --gpus 4 --num_workers 16 --batch_size 4 --learning_rate 0.01 --gan_weight 0.5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_8 --gpus 4 --num_workers 16 --batch_size 8 --learning_rate 0.01 --gan_weight 0.5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_16 --gpus 4 --num_workers 16 --batch_size 16 --learning_rate 0.01 --gan_weight 0.5
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_2 --gpus 4 --num_workers 16 --batch_size 2 --learning_rate 0.0001 --gan_weight 0.5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_4 --gpus 4 --num_workers 16 --batch_size 4 --learning_rate 0.0001 --gan_weight 0.5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_8 --gpus 4 --num_workers 16 --batch_size 8 --learning_rate 0.0001 --gan_weight 0.5
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_16 --gpus 4 --num_workers 16 --batch_size 16 --learning_rate 0.0001 --gan_weight 0.5
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_2 --gpus 4 --num_workers 16 --batch_size 2 --learning_rate 0.01 --gan_weight 1.0
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_4 --gpus 4 --num_workers 16 --batch_size 4 --learning_rate 0.01 --gan_weight 1.0
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_8 --gpus 4 --num_workers 16 --batch_size 8 --learning_rate 0.01 --gan_weight 1.0
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_16 --gpus 4 --num_workers 16 --batch_size 16 --learning_rate 0.01 --gan_weight 1.0
+
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_2 --gpus 4 --num_workers 16 --batch_size 2 --learning_rate 0.0001 --gan_weight 1.0
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_4 --gpus 4 --num_workers 16 --batch_size 4 --learning_rate 0.0001 --gan_weight 1.0
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_8 --gpus 4 --num_workers 16 --batch_size 8 --learning_rate 0.0001 --gan_weight 1.0
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py --name batch_16 --gpus 4 --num_workers 16 --batch_size 16 --learning_rate 0.0001 --gan_weight 1.0
